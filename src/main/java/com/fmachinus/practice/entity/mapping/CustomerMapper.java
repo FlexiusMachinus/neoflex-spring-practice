8f6aa42d875e44c6b80772aeec0622d4f6f315d3
Flexius Machinus <flexiusmachinus@gmail.com>
2021-07-06 18:53:04 +0300

    Change the component model, add methods for collection mapping

--- src/main/java/com/fmachinus/practice/entity/mapping/CustomerMapper.java
+++ src/main/java/com/fmachinus/practice/entity/mapping/CustomerMapper.java
@@ -3,13 +3,17 @@ package com.fmachinus.practice.entity.mapping;
 import com.fmachinus.practice.entity.Customer;
 import com.fmachinus.practice.entity.dto.CustomerDto;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
-@Mapper
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface CustomerMapper {
 
-    CustomerMapper MAPPER = Mappers.getMapper(CustomerMapper.class);
-
     CustomerDto fromCustomer(Customer customer);
+
     Customer toCustomer(CustomerDto customerDto);
+
+    List<CustomerDto> fromCustomerCollection(List<Customer> customerList);
+
+    List<Customer> toCustomerCollection(List<CustomerDto> customerDtoList);
 }
