68aa232ba8e5cd5b71c4a4b519c271c077f2cb72
Flexius Machinus <flexiusmachinus@gmail.com>
2021-07-07 12:24:26 +0300

    Move dependency injection to setters

--- src/main/java/com/fmachinus/practice/dto/mapping/OrderMapper.java
+++ src/main/java/com/fmachinus/practice/dto/mapping/OrderMapper.java
@@ -1,11 +1,12 @@
 package com.fmachinus.practice.dto.mapping;
 
+import com.fmachinus.practice.dto.OrderDto;
 import com.fmachinus.practice.entity.Customer;
 import com.fmachinus.practice.entity.Order;
 import com.fmachinus.practice.entity.Product;
-import com.fmachinus.practice.dto.OrderDto;
 import com.fmachinus.practice.service.CustomerService;
 import com.fmachinus.practice.service.ProductService;
+import lombok.NoArgsConstructor;
 import org.mapstruct.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,13 +14,22 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
+@NoArgsConstructor
 @Mapper(componentModel = "spring", uses = { CustomerMapper.class, ProductMapper.class, CustomerService.class, ProductService.class })
 public abstract class OrderMapper {
 
-    @Autowired
     private CustomerService customerService;
-    @Autowired
     private ProductService productService;
+
+    @Autowired
+    public void setCustomerService(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
 
     public abstract OrderDto fromOrder(Order order);
 
